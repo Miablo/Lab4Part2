92fdda50f360ec6d329baa4a73908766c26121f9
Miablo <themio@themioshow.com>
2021-02-15 19:59:04 -0500

    added main

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+/**
+ * (3 pts) Given a JUnitIT.zip file as the program for test,
+ * implement the function foo in Q1 and utilize JUnit to perform “Robustness testing”.
+ * Assert the correctness between the expected output and the actual output.
+ */
+public class Main {
+}
