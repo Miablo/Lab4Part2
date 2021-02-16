8fb1d73201dcf31a3dfa792b0cd4bb220b3c9596
Pcow9 <71295830+Pcow9@users.noreply.github.com>
2021-02-15 22:38:55 -0500

    Add some comments

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,22 @@
-/**
+/*
+ *
+ * @ author Cody Walker,
+ * @ version 1.0
  * (3 pts) Given a JUnitIT.zip file as the program for test,
  * implement the function foo in Q1 and utilize JUnit to perform “Robustness testing”.
  * Assert the correctness between the expected output and the actual output.
  */
 public class Main {
     public String triangleType(int a, int b, int c){
-        if(a == 0 || b == 0 || c == 0){
+        if(a == 0 || b == 0 || c == 0){ // first see if it even is a triangle
             return "Not a triangle";
         }else{
-            if(a == b && a == c){
+            if(a == b && a == c){  // checks if it is an equilateral
                 return "equilateral";
-            }else if(a == b || a == c || b == c){
+            }else if(a == b || a == c || b == c){  // check if it is isosceles
                 return "isosceles";
             }else{
-                return "scalene";
+                return "scalene"; //if neither will return scalene
             }
         }
     }
